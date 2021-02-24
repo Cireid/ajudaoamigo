9dde54c33e76f3910ea252c92660873febff70df
Gabriel  Dieric <gabrieldieric@gmail.com>
2021-02-24 19:23:53 -0300

    first

---
+++ resources/views/pages/table.blade.php
@@ -0,0 +1,113 @@
+@include('layout.app')
+
+
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">CPF/CNPJ</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Nome Fantasia</th>
+            <th scope="col">Logradouro</th>
+            <th scope="col">Complemento</th>
+            <th scope="col">Bairro</th>
+            <th scope="col">Nome do Municipio</th>
+            <th scope="col">UF</th>
+            <th scope="col">CEP</th>
+            <th scope="col">País</th>
+            <th scope="col">Fone</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">09236632000123</th>
+            <td>ULTRA COMERCIO E DISTRIBUICAO LTDA</td>
+            <td>ULTRA LTDA</td>
+            <td>MARIA PRESOTTO PUCCI, 300</td>
+            <td>GALPAO 1</td>
+            <td>DISTRITO INDUSTRIAL</td>
+            <td>JOAO PESSOA</td>
+            <td>PB</td>
+            <td>58082011</td>
+            <td>BRASIL</td>
+            <td>8332316500</td>
+        </tr>
+        <tr>
+            <th scope="row">00000000000</th>
+            <td>JOAQUIN DA SILVA PINTO</td>
+            <td>PARADIA DOIS IRMÃOS</td>
+            <td>RUA DOS BOBOS, 0</td>
+            <td>CASA</td>
+            <td>EXPEDICIONARIOS</td>
+            <td>JOAO PESSOA</td>
+            <td>PB</td>
+            <td>58040-000</td>
+            <td>BRASIL</td>
+            <td>83999999999</td>
+        </tr>
+        <tr>
+            <th scope="row">00000000000000</th>
+            <td>JOÃOZINHO23SILVA</td>
+            <td>JOÃOZINHOGAMEPLAY</td>
+            <td>25 DE MARÇO, 20</td>
+            <td>CASA</td>
+            <td>SÃO PAULO</td>
+            <td>SÃO PAULO</td>
+            <td>SP</td>
+            <td>50000-000</td>
+            <td>BRASIL</td>
+            <td>8397777777</td>
+        </tr>
+        <tr>
+            <th scope="row">00000000000000</th>
+            <td>JOÃOZINHO23SILVA</td>
+            <td>JOÃOZINHOGAMEPLAY</td>
+            <td>25 DE MARÇO, 20</td>
+            <td>CASA</td>
+            <td>SÃO PAULO</td>
+            <td>SÃO PAULO</td>
+            <td>SP</td>
+            <td>50000-000</td>
+            <td>BRASIL</td>
+            <td>8397777777</td>
+        </tr>
+        <tr>
+            <th scope="row">00000000000000</th>
+            <td>JOÃOZINHO23SILVA</td>
+            <td>JOÃOZINHOGAMEPLAY</td>
+            <td>25 DE MARÇO, 20</td>
+            <td>CASA</td>
+            <td>SÃO PAULO</td>
+            <td>SÃO PAULO</td>
+            <td>SP</td>
+            <td>50000-000</td>
+            <td>BRASIL</td>
+            <td>8397777777</td>
+        </tr>
+        <tr>
+            <th scope="row">00000000000000</th>
+            <td>JOÃOZINHO23SILVA</td>
+            <td>JOÃOZINHOGAMEPLAY</td>
+            <td>25 DE MARÇO, 20</td>
+            <td>CASA</td>
+            <td>SÃO PAULO</td>
+            <td>SÃO PAULO</td>
+            <td>SP</td>
+            <td>50000-000</td>
+            <td>BRASIL</td>
+            <td>8397777777</td>
+        </tr>
+        <tr>
+            <th scope="row">00000000000000</th>
+            <td>JOÃOZINHO23SILVA</td>
+            <td>JOÃOZINHOGAMEPLAY</td>
+            <td>25 DE MARÇO, 20</td>
+            <td>CASA</td>
+            <td>SÃO PAULO</td>
+            <td>SÃO PAULO</td>
+            <td>SP</td>
+            <td>50000-000</td>
+            <td>BRASIL</td>
+            <td>8397777777</td>
+        </tr>
+    </tbody>
+</table>
---
+++ resources/views/pages/tasks.blade.php
@@ -0,0 +1,174 @@
+@include('layout.app')
+
+<div class="container-fluid">
+    <div class="card mt-5">
+        <div class="card-body pColor">
+            <div class="row mb-5 justify-content-center">
+                <div class="input-group iSize">
+                    <input type="file" class="form-control" id="inputGroupFile01">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="TAG">
+                </div>
+                <div class="col-4">
+                    <input type="text" class="form-control" placeholder="Descrição">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="NCM">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="O/CST">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="CFOP" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="Vlr.Total" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="BC.Icms" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="Vlr.Icms" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="Aliq." >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="Desc." >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="ST" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="IPI" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="Outros" >
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="card mt-4">
+        <div class="card-body sColor">
+            <div class="row justify-content-center mb-5">
+                <div class="input-group iSize">
+                    <input type="file" class="form-control" id="inputGroupFile01">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="TAG2">
+                </div>
+                <div class="col-4">
+                    <input type="text" class="form-control" placeholder="Descrição2">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="NCM2">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="O/CST2">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="CFOP2" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="Vlr.Total2" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="BC.Icms2" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="Vlr.Icms2" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="Aliq.2" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="Desc.2" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="ST2" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="IPI2" >
+                </div>
+            </div>
+
+            <div class="row mt-5 justify-content-center">
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="Base">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="CST_PIS">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="Aliq_Pi">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="CST_Cofi">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="Aliq_Confin" >
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card mt-4">
+        <div class="card-body sColor">
+            <div class="row">
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="C170">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="Valor Contail">
+                </div>
+                <div class="col-4">
+                    <input type="text" class="form-control" placeholder="Valor Produto">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="Desconto">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="BC.Icms">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="Vlr.Icms" >
+                </div>
+                <div class="col-2 mt-2">
+                    <input type="text" class="form-control" placeholder="Despesa" >
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="C190">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="Valor Operação">
+                </div>
+                <div class="col-4">
+                    <input type="text" class="form-control" placeholder="CFOP">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="CST">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="BC.Icms2">
+                </div>
+                <div class="col-2">
+                    <input type="text" class="form-control" placeholder="Vlr.Icms2" >
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
